613db0d090f6e9f30b6092341d9e082661c202f9
navneet <uncrownedking010@gmail.com>
2026-06-04 13:21:51 +0530

    test

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World from GitHub Actions test!");
+    }
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
